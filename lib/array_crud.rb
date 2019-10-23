487ad00ec0b32ac464219013570aed72f700b747
Ajani Queeley <ajaniqueeley@gmail.com>
2019-10-23 20:29:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,46 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array.push(element)
-  add_element_to_end_of_array(my_array, 'blue')
+
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array.unshift(element)
-  add_element_to_start_of_array(my_array, 'blue')
+
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array.pop
-  remove_element_from_end_of_array(my_array)
+
 end
 
 def remove_element_from_start_of_array(array)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array.shift
-  remove_element_from_start_of_array(my_array)
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  retrieve_element_from_index(my_array,[2])
+  
 end
 
 def retrieve_first_element_from_array(array)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array[-1]
+
 end
 
 def update_element_from_index(array, index_number, element)
-  my_array = ['red', 'yellow', 'orange', 'green']
-  my_array[3] = 'magenta'
-  update_element_from_index(my_array,[3],'magenta')
+
 end
